2307574d7b7c829ac98126c39225858f7cd14f21
Izzy Cecil <lorr.cecil@gmail.com>
2010-08-27 21:47:52 -0500

    --DB Portability--     The DB now is given with the os.path.abspath(), so it can be portable.

--- Seminar_Django/settings.py
+++ Seminar_Django/settings.py
@@ -1,5 +1,7 @@
 # Django settings for Seminar_Django project.
 
+from os import path
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -10,7 +12,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = '/Users/tyler/Documents/Programming/Seminar-Tracker/Seminar_Django/DB/tracker.db'             # Or path to database file if using sqlite3.
+DATABASE_NAME = path.abspath('DB/tracker.db')
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
