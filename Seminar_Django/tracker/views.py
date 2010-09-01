416cb229afa448f520706c07ede50a4b5857f8a6
Izzy Cecil <lorr.cecil@gmail.com>
2010-08-31 19:23:35 -0500

    --accounts/profile/--   Redirects to an actual page now. It will return the user to login if something has gone wrong. Other wise, it gives the user a freindly greeting

--- Seminar_Django/tracker/views.py
+++ Seminar_Django/tracker/views.py
@@ -1,6 +1,12 @@
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse , HttpResponseRedirect
 from django.shortcuts import render_to_response
 
 def test_veiw(request):
 	return render_to_response('test.dhtml')
+	
+def roster(request):
+	if not request.user.is_authenticated():
+		return HttpResponseRedirect('/accounts/login/')
+	else:
+		return HttpResponse('ok dawg')
