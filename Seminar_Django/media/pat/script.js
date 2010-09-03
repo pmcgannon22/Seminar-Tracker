c765d721d3e4fe8fc9b2067240a8ad2d2fd811c9
Izzy Cecil <lorr.cecil@gmail.com>
2010-09-03 18:16:16 -0500

    --File Structure Change--   Tempate folder is now inside of jango project, with th db folder. In addition, a media folder has been added. This folder contains our javascript lib, as well as patricks css and home page. The home page has been added to the templates folder   The settings have also been cleaned up a tad.

---
+++ Seminar_Django/media/pat/script.js
@@ -0,0 +1,7 @@
+$(function () {
+		var availableTags = ["Patrick McGannon","Spencer Brown","Tyler Cecil","Russell White"];
+		$("#search").autocomplete({
+			source: availableTags
+		)};
+	});
+	
--- Seminar_Django/settings.py
+++ Seminar_Django/settings.py
@@ -1,5 +1,4 @@
 # Django settings for Seminar_Django project.
-
 from os import path
 
 DEBUG = True
@@ -8,14 +7,14 @@ TEMPLATE_DEBUG = DEBUG
 ADMINS = (
     # ('Your Name', 'your_email@domain.com'),
 )
+MANAGERS = ADMINS
 
+ROOT_PATH = path.dirname(__file__)
 #tells Django to merge Teacher and User Objects
 AUTH_PROFILE_MODULE = 'tracker.Teacher'
 
-MANAGERS = ADMINS
-
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = path.abspath('DB/tracker.db')
+DATABASE_NAME = path.join (ROOT_PATH, 'db/tracker.db')
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
@@ -40,12 +39,12 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT =  path.join(ROOT_PATH, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = 'http://127.0.0.1:8000/media/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
@@ -74,7 +73,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    path.join(path.basename(__file__), '../../Templates'),
+    path.join(ROOT_PATH, 'templates'),
     #'/Volumes/mac hd/Users/tyler/Documents/Programming/Seminar-Tracker/Templates/',
 )
 
